d78dc8add58d55114fb829e7e8ae05e005e19bfc
Diyan Lyubchev <diyan.lyubchev@gmail.com>
2020-02-21 14:40:58 +0200

    separate js file

---
+++ MoviesApi/MoviesApi/wwwroot/js/navigationButtons.js
@@ -0,0 +1,16 @@
+﻿function RefreshMovie() {
+    window.location.href = '/Home/GetNewMovies'
+}
+
+
+function ShowMyMovie() {
+    window.location.href = '/Movie/ShowMyMovie'
+}
+
+function ShowWachedMovie() {
+    window.location.href = '/Movie/GetMyAllRateAndReviewedMovies'
+}
+
+function GoHome() {
+    window.location.href = '/Home/Index'
+}
--- MoviesApi/MoviesApi/wwwroot/js/site.js
+++ MoviesApi/MoviesApi/wwwroot/js/site.js
@@ -21,23 +21,6 @@ function ReviewedMovie(id) {
     });
 }
 
-function RefreshMovie() {
-    window.location.href = '/Home/GetNewMovies'
-}
-
-
-function ShowMyMovie() {
-    window.location.href = '/Movie/ShowMyMovie'
-}
-
-function ShowWachedMovie() {
-    window.location.href =  '/Movie/GetMyAllRateAndReviewedMovies'
-}
-
-function GoHome() {
-    window.location.href = '/Home/Index'
-}
-
 $(':radio').change(function () {
     const rate = this.value;
     
